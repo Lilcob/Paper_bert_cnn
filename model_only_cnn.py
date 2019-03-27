23f741854a3f9d3a6e29252f06b06d36a8c283d0
lilcob <yts1993@kore.ac.kr>
2019-03-27 01:48:41 -0700

    Initial commit

---
+++ model_only_cnn.py
@@ -0,0 +1,75 @@
+from keras.layers import Input, Dense, Embedding, Conv2D, MaxPool2D
+from keras.layers import Reshape, Flatten, Dropout, Concatenate
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+from keras.models import Model
+#from sklearn.model_selection import train_test_split
+from data_helpers import load_data
+
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import Conv1D, GlobalMaxPooling1D
+from keras.datasets import imdb
+
+# set parameters:
+max_features = 50000
+maxlen = 400
+batch_size = 32
+embedding_dims = 50
+filters = 250
+kernel_size = 3
+hidden_dims = 250
+epochs = 2
+
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+
+sequence_length = x_train.shape[1] # 56
+vocabulary_size = max_features # 18765
+embedding_dim = 256
+filter_sizes = [3,4,5]
+num_filters = 512
+drop = 0.5
+
+epochs = 100
+batch_size = 30
+
+# this returns a tensor
+print("Creating Model...")
+inputs = Input(shape=(sequence_length,), dtype='int32')
+embedding = Embedding(input_dim=vocabulary_size, output_dim=embedding_dim, input_length=sequence_length)(inputs)
+reshape = Reshape((sequence_length,embedding_dim,1))(embedding)
+
+conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+
+maxpool_0 = MaxPool2D(pool_size=(sequence_length - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
+maxpool_1 = MaxPool2D(pool_size=(sequence_length - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
+maxpool_2 = MaxPool2D(pool_size=(sequence_length - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
+
+
+
+
+concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+flatten = Flatten()(concatenated_tensor)
+dropout = Dropout(drop)(flatten)
+output = Dense(units=1, activation='softmax')(dropout)
+
+# this creates a model that includes
+model = Model(inputs=inputs, outputs=output)
+
+checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
+adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+print("Traning Model...")
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[checkpoint], validation_data=(x_test, y_test))  # starts training
---
+++ model_only_hat.py
@@ -0,0 +1,197 @@
+import numpy as np
+import pandas as pd
+import _pickle as cPickle
+from collections import defaultdict
+import re
+
+from bs4 import BeautifulSoup
+
+import sys
+import os
+
+
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+
+from keras.layers import Embedding
+from keras.layers import Dense, Input, Flatten
+from keras.layers import Conv1D, MaxPooling1D, Embedding, merge, Dropout, LSTM, GRU, Bidirectional, TimeDistributed
+from keras.models import Model
+
+from keras import backend as K
+from keras.engine.topology import Layer, InputSpec
+from keras import initializers
+
+MAX_SENT_LENGTH = 100
+MAX_SENTS = 15
+MAX_NB_WORDS = 20000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2
+
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for dataset
+    Every dataset is lower cased except
+    """
+    string = re.sub(r"\\", "", string)
+    string = re.sub(r"\'", "", string)
+    string = re.sub(r"\"", "", string)
+    return string.strip().lower()
+
+
+data_train = pd.read_csv('labeledTrainData.tsv', sep='\t')
+print(data_train.shape)
+
+from nltk import tokenize
+import nltk
+nltk.download('punkt')
+reviews = []
+labels = []
+texts = []
+
+for idx in range(data_train.review.shape[0]):
+    text = BeautifulSoup(data_train.review[idx])
+    text = clean_str(text.get_text())
+    texts.append(text)
+    sentences = tokenize.sent_tokenize(text)
+    reviews.append(sentences)
+
+    labels.append(data_train.sentiment[idx])
+
+tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
+tokenizer.fit_on_texts(texts)
+
+data = np.zeros((len(texts), MAX_SENTS, MAX_SENT_LENGTH), dtype='int32')
+
+for i, sentences in enumerate(reviews):
+    for j, sent in enumerate(sentences):
+        if j < MAX_SENTS:
+            wordTokens = text_to_word_sequence(sent)
+            k = 0
+            for _, word in enumerate(wordTokens):
+                if k < MAX_SENT_LENGTH and tokenizer.word_index[word] < MAX_NB_WORDS:
+                    data[i, j, k] = tokenizer.word_index[word]
+                    k = k + 1
+
+word_index = tokenizer.word_index
+print('Total %s unique tokens.' % len(word_index))
+
+labels = to_categorical(np.asarray(labels))
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+x_train = data[:-nb_validation_samples]
+y_train = labels[:-nb_validation_samples]
+x_val = data[-nb_validation_samples:]
+y_val = labels[-nb_validation_samples:]
+
+print('Number of positive and negative reviews in traing and validation set')
+print(y_train.sum(axis=0))
+print(y_val.sum(axis=0))
+
+GLOVE_DIR = "."
+embeddings_index = {}
+f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Total %s word vectors.' % len(embeddings_index))
+
+embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+
+
+# building Hierachical Attention network
+embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+
+embedding_layer = Embedding(len(word_index) + 1,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=MAX_SENT_LENGTH,
+                            trainable=True,
+                            mask_zero=True)
+
+
+class AttLayer(Layer):
+    def __init__(self, attention_dim):
+        self.init = initializers.get('normal')
+        self.supports_masking = True
+        self.attention_dim = attention_dim
+        super(AttLayer, self).__init__()
+
+    def build(self, input_shape):
+       # assert len(input_shape) == 3
+        self.W = K.variable(self.init((input_shape[-1], self.attention_dim)))
+        self.b = K.variable(self.init((self.attention_dim, )))
+        self.u = K.variable(self.init((self.attention_dim, 1)))
+        self.trainable_weights = [self.W, self.b, self.u]
+        super(AttLayer, self).build(input_shape)
+
+    def compute_mask(self, inputs, mask=None):
+        return mask
+
+    def call(self, x, mask=None):
+        # size of x :[batch_size, sel_len, attention_dim]
+        # size of u :[batch_size, attention_dim]
+        # uit = tanh(xW+b)
+        uit = K.tanh(K.bias_add(K.dot(x, self.W), self.b))
+        ait = K.dot(uit, self.u)
+        ait = K.squeeze(ait, -1)
+
+        ait = K.exp(ait)
+
+        if mask is not None:
+            # Cast the mask to floatX to avoid float64 upcasting in theano
+            ait *= K.cast(mask, K.floatx())
+        ait /= K.cast(K.sum(ait, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+        ait = K.expand_dims(ait)
+        weighted_input = x * ait
+        output = K.sum(weighted_input, axis=1)
+
+        return output
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[-1])
+
+
+sentence_input = Input(shape=(MAX_SENT_LENGTH,), dtype='float')
+embedded_sequences = embedding_layer(sentence_input)
+l_lstm = Bidirectional(GRU(100, return_sequences=True))(embedded_sequences)
+l_att = AttLayer(100)(l_lstm)
+sentEncoder = Model(sentence_input, l_att)
+
+review_input = Input(shape=(MAX_SENTS, MAX_SENT_LENGTH), dtype='float')
+review_encoder = TimeDistributed(sentEncoder)(review_input)
+l_lstm_sent = Bidirectional(GRU(100, return_sequences=True))(review_encoder)
+l_att_sent = AttLayer(100)(l_lstm_sent)
+preds = Dense(2, activation='softmax')(l_att_sent)
+model = Model(review_input, preds)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['acc'])
+
+print("model fitting - Hierachical attention network")
+model.fit(x_train, y_train, validation_data=(x_val, y_val),
+          nb_epoch=10, batch_size=50)
